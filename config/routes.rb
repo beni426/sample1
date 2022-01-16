c82bfb0f52acd45275ac085202da840a50dd66ba
Ryu <goskn88006@yahoo.co.jp>
2022-01-16 17:38:03 +0900

    投稿確認が出ない

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get '/blogs',to: 'blogs#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/',to:"blogs#index"
+   resources :blogs do
+      collection do
+         post :confirm
+    end
+  end
 end
